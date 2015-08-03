553d13f7df61423a9b55e4c0b94a878211a59164
Peter Tribble <peter.tribble@gmail.com>
2015-08-03 20:40:10 +0100

    Use os.uptime() for portability

--- monitors/uptime.js
+++ monitors/uptime.js
@@ -1,16 +1,9 @@
 var os = require('os');
-var exec = require('child_process').exec;
 
 var Uptime = {};
 
 Uptime.monitor = function(history, callback) {
-  exec('ps -eo lstart,args | grep /sbin/init', function(error, result) {
-    var processTextIndex = result.indexOf('/sbin/init');
-    var dateText = result.substring(0, processTextIndex);
-    var uptime = Date.now() - new Date(dateText).getTime();
-
-    callback(uptime);
-  });
+    callback(1000*(os.uptime()));
 };
 
 Uptime.history = false;
